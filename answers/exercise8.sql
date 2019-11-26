0caed5a0a46f3d2aaf86a2855a885b9f61393958
michael-doll <MWDoll88@gmail.com>
2019-11-25 22:39:02 -0500

    save work, ex8

---
+++ answers/exercise8.sql
@@ -0,0 +1,21 @@
+-- Table didn't exist
+-- CREATE TABLE Students(
+-- StudentName VARCHAR,
+-- Address VARCHAR, 
+-- City VARCHAR, 
+-- PostalCode VARCHAR,
+-- Country VARCHAR
+-- );  
+INSERT INTO Students(
+StudentName,
+Address, 
+City, 
+PostalCode,
+Country)
+VALUES (
+'Jane',
+'57 Union St',
+'Glasgow',
+'G13RB', 
+'Scotland'
+);
